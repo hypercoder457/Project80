97ac697561963c37431f6156d87d42ebd2ac2bf0
rpshende <rpshende@DELL-PRECISION5000>
2022-04-16 12:50:31 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from "./screens/Home";
+import StarMapScreen from "./screens/StarMap";
+import DailyPicScreen from "./screens/DailyPic";
+import SpaceCraftScreen from "./screens/SpaceCraft";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="StarMap" component={StarMapScreen} />
+        <Stack.Screen name="DailyPic" component={DailyPicScreen} />
+        <Stack.Screen name="SpaceCraft" component={SpaceCraftScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
